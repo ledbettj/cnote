8d6a98cefb6d1c34cfbf17ed5e66cc9fc1b4b2d5
John Ledbetter <john.ledbetter@gmail.com>
2011-05-24 11:47:31 -0400

    cleaned up filenames

--- cnote/__init__.py
+++ cnote/__init__.py
@@ -1,9 +1,9 @@
-from notify_manager import NotificationManager
-from notify_service import NotificationService
+from manager import NotificationManager
+from service import NotificationService
 
-from notify_window import NotificationWindow
+from window import NotificationWindow
 from theme import ThemeManager, Theme
-from notify_service import Notification
+from service import Notification
 
 import util
 import cairo_blur
